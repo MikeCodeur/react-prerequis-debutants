1e6011521eaeedec9232db48616e883685cc6345
Mike <contact@mikecodeur.com>
2021-07-30 09:04:10 +0800

    test

---
+++ src/__tests__/00.js
@@ -0,0 +1,19 @@
+import {robertAlertTip} from '@mikecodeur/react-course-app/test-utils'
+import chalk from 'chalk'
+import {render, screen, prettyDOM} from '@testing-library/react'
+
+//import App from '../final/02'
+//import App from '../final/02.bonus-1'
+//import App from '../exercise/02'
+
+test('Just a template test', () => {
+  robertAlertTip(
+    () => {
+      expect(true).toEqual(true)
+    },
+    () =>
+      `Il manque le fontStyle : 'italic' sur ce container`.trim(),
+  )
+
+ 
+})
