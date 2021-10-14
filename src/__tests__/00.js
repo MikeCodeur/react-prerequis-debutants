aaf5d61d48a6ec74d19e246d37527f2c22c89ee3
Mike Codeur <cabanas.mickael@gmail.com>
2021-10-14 10:52:56 +0800

    init

---
+++ src/__tests__/00.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import {robertAlertTip} from '@mikecodeur/react-course-app/test-utils'
+import chalk from 'chalk'
+import {render, screen, prettyDOM} from '@testing-library/react'
+
+//import App from '../final/02'
+//import App from '../final/02.bonus-1'
+//import App from '../exercise/02'
+
+test('Just a template test', () => {
+  robertAlertTip(
+    () => {
+      expect(true).toEqual(true)
+    },
+    () =>
+      `Il manque le fontStyle : 'italic' sur ce container`.trim(),
+  )
+
+ 
+})
